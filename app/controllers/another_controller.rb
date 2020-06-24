92874040033c7124359370d47aa9c2dc3bfb48d1
Abdul Mumin Seidu <abmumseidu@gmail.com>
2020-06-24 21:15:17 +0000

    Add a migration using rails g

---
+++ app/controllers/another_controller.rb
@@ -0,0 +1,16 @@
+class AnotherController < ApplicationController
+  def index
+
+  	@greeting = "Good Evening"
+  	@sum = 17 + 1
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def hello
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'another/index'
+  get 'another/update'
+  get 'another/edit'
+  get 'another/hello'
   get 'play/index'
+
+  # get ':controller(/:action(/:id))'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
